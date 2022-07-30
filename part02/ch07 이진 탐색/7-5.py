528413106c44922910f0b38f0c579bddec6da671
usiohc <asltn99@gmail.com>
2022-07-30 15:07:41 +0900

    binary_search v1.2

--- part02/ch07 이진 탐색/7-5.py
+++ part02/ch07 이진 탐색/7-5.py
@@ -1,3 +1,6 @@
+# ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+# binary_search
+
 def binary_search(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
@@ -25,3 +28,41 @@ for m in mNums:
         print('yes', end = ' ')
     else:
         print('no', end = ' ')
+
+
+# ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+# 계수 정렬
+
+n = int(input())
+array = [0] * 1000001
+
+for i in input().split():
+    array[int(i)] = 1
+
+m = int(input())
+
+x = list(map(int, input().split()))
+
+for i in x:
+    if array[i] == 1:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+
+
+# ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+# 집합 자료형 이용
+
+n = int(input())
+array = set(map(int, input().split()))
+
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
