783f7f17c4ce41f17dea9954f83c810ad95b2130
usiohc <asltn99@gmail.com>
2023-02-16 17:23:12 +0900

    graph algorithms v1.1

---
+++ part02/ch10 그래프 이론/10-7.py
@@ -0,0 +1,50 @@
+import sys
+input = sys.stdin.readline
+
+
+# o == 1
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# o == 0
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+
+parent = [i for i in range(n+1)]
+
+
+for _ in range(m):
+    o, a, b = map(int, input().split())
+    
+    # find 1
+    if o:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print('YES')
+        else:
+            print('NO')
+    # union 0
+    else:
+        union_parent(parent, a, b)
+
+# 7 8
+# 0 1 3
+# 1 1 7
+# NO
+# 0 7 6
+# 1 7 1
+# NO
+# 0 3 7
+# 0 4 2
+# 0 1 1
+# 1 1 1
+# YES
