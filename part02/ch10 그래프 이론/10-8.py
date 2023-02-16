8969361e30981e9a0454d1da0030a210af58f698
usiohc <asltn99@gmail.com>
2023-02-16 17:52:17 +0900

    graph algorithms v1.2

---
+++ part02/ch10 그래프 이론/10-8.py
@@ -0,0 +1,34 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = [i for i in range(n+1)]
+
+edges = []
+result = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+max_cost = -1
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        max_cost = cost
+
+print(result - max_cost)
